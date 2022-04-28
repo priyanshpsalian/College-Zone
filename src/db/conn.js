a6c667c05ee2a74ea21e97cb7f5ccd55db0d7010
Idris110 <ratlamwalaidris110@gmail.com>
2022-04-29 01:04:27 +0530

    Better navbar

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 mongoose
-  // .connect("mongodb+srv://user1:1234@clubzone.12dvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    .connect("mongodb://localhost:27017/ClubZone", {  
-  useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect("mongodb+srv://user1:1234@clubzone.12dvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+// .connect("mongodb://localhost:27017/ClubZone", {  
+  // useNewUrlParser: true,
+    // useUnifiedTopology: true,
     // useCreateIndex: true,
   })
   .then(() => {
-    console.log(`connection is succesful`);
+    console.log(`connection is successful`);
   })
   .catch((e) => {
     console.log(`no connection`, e);
