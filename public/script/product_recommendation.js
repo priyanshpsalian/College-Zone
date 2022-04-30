b3671f804f5f9e0b908478dbf3c4b15c48736315
Idris110 <ratlamwalaidris110@gmail.com>
2022-05-01 04:53:58 +0530

    aur kitna karu

--- public/script/product_recommendation.js
+++
@@ -1,8 +0,0 @@
-// var cityList = JSON.parse(list);
-// console.log(cityList);
-
-function aa() {
-  fetch("/product_recommendation")
-    .then((response) => response.json())
-    .then((data) => console.log(data, "hh"));
-}
